a9bf80953b930e324035e3cb4c1b7c06ab73cdc2
treneese <treneesejohnson9012@gmail.com>
2024-04-03 14:59:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+//returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+//const
+//assign an anonymous function to it
+//return a new array containing the first two drivers in the array:
+
+//returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+//selectingDrivers
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+
+const createFareMultiplier = function(multiplyValue) {
+    return function (value){
+return multiplyValue * value;
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, driversToReturn){
+  return driversToReturn(drivers);
+}
